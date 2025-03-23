58ad54b1d06710c9e4f26789474f90b18319f761
Naiizey <nairolfdu22@gmail.com>
2025-03-23 01:45:28 +0100

    db + getChamps in DB

---
+++ server/db.sql
@@ -0,0 +1,130 @@
+CREATE SEQUENCE arenaCheckerIndexes START 1;
+
+CREATE SCHEMA "arenaChampTracker";
+
+CREATE TABLE "arenaChampTracker".champions (
+    champion_id integer,
+    name varchar(50),
+    image varchar(50),
+    primary key (champion_id)
+);
+
+CREATE TABLE "arenaChampTracker".players (
+    player_id integer,
+    name varchar(16),
+    primary key (player_id)
+);
+
+CREATE TABLE "arenaChampTracker".stats (
+    player_id integer,
+    champion_id integer,
+    win boolean,
+    primary key (player_id, champion_id)
+);
+
+ALTER TABLE "arenaChampTracker".stats
+ADD CONSTRAINT stats_player_id_fk FOREIGN KEY (player_id) REFERENCES "arenaChampTracker".players (player_id);
+
+ALTER TABLE "arenaChampTracker".stats
+ADD CONSTRAINT stats_champion_id_fk FOREIGN KEY (champion_id) REFERENCES "arenaChampTracker".champions (champion_id);
+
+CREATE OR REPLACE VIEW "arenaChampTracker".v_champions AS (
+    SELECT *
+    FROM "arenaChampTracker".champions
+);
+
+CREATE
+OR REPLACE RULE insert_v_champions AS ON INSERT TO "arenaChampTracker".v_champions DO INSTEAD (
+    INSERT INTO
+        "arenaChampTracker".champions (champion_id, name, image)
+    VALUES (
+            nextval('arenacheckerindexes'),
+            new.name,
+            new.image
+        )
+    RETURNING
+        *
+);
+
+CREATE
+OR REPLACE RULE delete_v_champions AS ON DELETE TO "arenaChampTracker".v_champions DO INSTEAD (
+    DELETE FROM "arenaChampTracker".champions
+    WHERE
+        champions.champion_id = champion_id
+    RETURNING
+        *
+);
+
+CREATE
+OR REPLACE RULE update_v_champions AS ON UPDATE TO "arenaChampTracker".v_champions DO INSTEAD (
+    UPDATE "arenaChampTracker".champions
+    SET
+        name = new.name,
+        image = new.image
+    RETURNING
+        *
+);
+
+CREATE OR REPLACE VIEW "arenaChampTracker".v_players AS (
+    SELECT *
+    FROM "arenaChampTracker".players
+);
+
+CREATE
+OR REPLACE RULE insert_v_players AS ON INSERT TO "arenaChampTracker".v_players DO INSTEAD (
+    INSERT INTO
+        "arenaChampTracker".players (player_id, name)
+    VALUES (
+            nextval('arenacheckerindexes'),
+            new.name
+        )
+    RETURNING
+        *
+);
+
+CREATE
+OR REPLACE RULE delete_v_players AS ON DELETE TO "arenaChampTracker".v_players DO INSTEAD (
+    DELETE FROM "arenaChampTracker".players
+    WHERE
+        players.player_id = player_id
+    RETURNING
+        *
+);
+
+CREATE
+OR REPLACE RULE update_v_players AS ON UPDATE TO "arenaChampTracker".v_players DO INSTEAD (
+    UPDATE "arenaChampTracker".players
+    SET
+        game_name = new.game_name,
+        tag_line = new.tag_line
+    RETURNING
+        *
+);
+
+CREATE OR REPLACE VIEW "arenaChampTracker".v_stats AS (
+    SELECT *
+    FROM "arenaChampTracker".stats
+);
+
+CREATE
+OR REPLACE RULE insert_v_stats AS ON INSERT TO "arenaChampTracker".v_stats DO INSTEAD (
+    INSERT INTO
+        "arenaChampTracker".stats (player_id, champion_id, win)
+    VALUES (
+            new.player_id,
+            new.champion_id,
+            new.win
+        )
+    RETURNING
+        *
+);
+
+CREATE
+OR REPLACE RULE delete_v_stats AS ON DELETE TO "arenaChampTracker".v_stats DO INSTEAD (
+    DELETE FROM "arenaChampTracker".stats
+    WHERE
+        stats.player_id = player_id
+        AND stats.champion_id = champion_id
+    RETURNING
+        *
+);
--- server/getLeagueChamps.js
+++ server/getLeagueChamps.js
@@ -1,2 +1,37 @@
 import pg from 'pg';
 import { decrypt } from '@tka85/dotenvenc'
+
+const { Client } = pg;
+
+await decrypt();
+
+const cli = new Client({
+    host: process.env.dbHost,
+    port: process.env.dbPort,
+    database: process.env.db,
+    user: process.env.dbUser,
+    password: process.env.dbPsswd
+});
+
+await cli.connect();
+
+async function registerChamps() {
+    const resp = await fetch('https://ddragon.leagueoflegends.com/cdn/15.6.1/data/en_US/champion.json');
+    const json = await resp.json();
+
+
+    const query = 'INSERT INTO "arenaChampTracker".v_champions(name, image) VALUES($1, $2)';
+
+    for(const champ in json.data) {
+        const values = [
+            json.data[champ].name,
+            json.data[champ].image.full
+        ];
+
+        await cli.query(query, values);
+    }
+};
+
+await registerChamps();
+
+await cli.end();
